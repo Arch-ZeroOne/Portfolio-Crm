9131d7a036e8c1753d841700e535878d77e4c212
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-25 22:15:46 +0800

    Added actions cell and action buttons

--- app/dashboard/client/columns.tsx
+++ app/dashboard/client/columns.tsx
@@ -2,6 +2,15 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 //* Defiinition of the column structure
 export type Payment = {
   id: string;
@@ -10,6 +19,7 @@ export type Payment = {
   email: string;
 };
 
+// todo Learn and document how this works
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "email",
@@ -32,5 +42,29 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "status",
     header: "Status",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit Request</DropdownMenuItem>
+            <DropdownMenuItem>Approve Request</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
